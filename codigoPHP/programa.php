be424db11cd71928a067ead051bddbb2a99ea05d
RicardoSantom <richard1978_1@hotmail.com>
2022-12-20 07:27:26 +0100

    Inclusión footer desde archivo externo

--- codigoPHP/programa.php
+++ codigoPHP/programa.php
@@ -90,13 +90,6 @@ if (isset($_REQUEST['detalle'])) {
                 </form>
             </article>
         </main>
-        <footer>
-            <p>2022-23  IES LOS SAUCES. <a href="../../../index.html" id="enlacePrincipal" title="Enlace a Index Principal">Ricardo Santiago Tomé</a> © Todos los derechos reservados</p>
-            <a href="https://github.com/RicardoSantom/208DWESLoginLogoffTema5" target="blank" id="github" title="RicardoSantom en GitHub">
-            </a>
-            <a href="https://www.linkedin.com/in/ricardo-santiago-tom%C3%A9/" id="linkedin" title="Ricardo Santiago Tomé en Linkedim" target="_blank"></a>
-            <a href="../../doc/curriculumRicardo.pdf" class="material-icons" title="Curriculum Vitae Ricardo Santiago Tomé" target="_blank" id="curriculum"><span class="material-icons md-18">face</span></a>
-            <a href="../../208DWESproyectoDWES/index.php" id="enlaceSecundario" title="Enlace a Index DWES">Index DWES</a>
-        </footer>
+        <?php include_once './footer.php'; ?>
     </body>
 </html>
